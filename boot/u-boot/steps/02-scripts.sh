3d22f81df5ee0f97e36b458e3f2c753732cc55aa
Maximilian Köhl <mail@koehlma.de>
2025-04-29 11:26:25 +0200

    Revert "chore: build u-boot in reproducible environment"
    This reverts commit a7ee01c555f9f349a6c8e5f404525b522d40c59e.

--- boot/u-boot/steps/02-scripts.sh
+++ boot/u-boot/steps/02-scripts.sh
@@ -6,7 +6,7 @@ function build_script() {
     local file=$1;
     local name=$2;
     ./tools/mkimage -A arm -O linux -T script -C none -n "${name}" -a 0 -e 0 \
-        -d "/build/outside/scripts/${file}.uboot.sh" "/build/outside/bin/${file}.scr"
+        -d "./scripts/${file}.uboot.sh" "./out/${file}.scr"
 }
 
 build_script boot "Rugpi First Stage"
