2644d79297bf4c4c0e0ecbebd4f851f798f353ae
Maximilian Köhl <mail@koehlma.de>
2025-06-22 10:12:17 +0200

    tests: add test for static delta updates

--- tests/run-update-tests.sh
+++ tests/run-update-tests.sh
@@ -9,11 +9,15 @@ mkdir build
 export RUGIX_DEV=true
 
 ./run-bakery bake bundle customized-arm64 --without-compression
+./run-bakery bake bundle customized-arm64-delta --without-compression
+./run-bakery bundler delta build/customized-arm64/system.rugixb build/customized-arm64-delta/system.rugixb build/delta.rugixb
+
 ./run-bakery test test-update-bundle
 ./run-bakery test test-update-image
 ./run-bakery test test-update-index
 ./run-bakery test test-update-index-http
 ./run-bakery test test-update-index-multi
+./run-bakery test test-update-static-delta
 
 ./run-bakery bundler bundle bundles/script-bundle build/script-bundle.rugixb
 ./run-bakery test test-update-script
