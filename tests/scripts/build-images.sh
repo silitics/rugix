780152b1897d20a4c29fc164e2eb2b3e40788be9
Maximilian Köhl <mail@koehlma.de>
2025-07-15 22:45:32 +0200

    ci: remove `.rugix` directory as root

--- tests/scripts/build-images.sh
+++ tests/scripts/build-images.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 ./run-bakery bake image tryboot-pi4
 ./run-bakery bake image tryboot
 
-rm -rf .rugix
+sudo rm -rf .rugix
 
 ./run-bakery bake image u-boot
 ./run-bakery bake image u-boot-armhf
 
-rm -rf .rugix
+sudo rm -rf .rugix
