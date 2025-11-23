0a5dbeff8585b1b57747adb2e2972d5aa9817c22
Maximilian Köhl <mail@koehlma.de>
2025-11-23 19:23:17 +0100

    tests: log test execution

--- tests/run-update-tests.sh
+++ tests/run-update-tests.sh
@@ -13,18 +13,27 @@ export RUGIX_DEV=true
 ./run-bakery bundler delta build/customized-arm64/system.rugixb build/customized-arm64-delta/system.rugixb build/delta.rugixb
 ./run-bakery bundler signatures sign build/customized-arm64/system.rugixb keys/signer.crt keys/signer.key build/customized-arm64-signed.rugixb
 
+echo "TEST: test-update-bundle"
 ./run-bakery test test-update-bundle
+echo "TEST: test-update-bundle-signed"
 ./run-bakery test test-update-bundle-signed
+echo "TEST: test-update-image"
 ./run-bakery test test-update-image
+echo "TEST: test-update-index"
 ./run-bakery test test-update-index
+echo "TEST: test-update-index-http"
 ./run-bakery test test-update-index-http
+echo "TEST: test-update-index-multi"
 ./run-bakery test test-update-index-multi
+echo "TEST: test-update-static-delta"
 ./run-bakery test test-update-static-delta
 
 ./run-bakery bundler bundle bundles/script-bundle build/script-bundle.rugixb
+echo "TEST: test-update-script"
 ./run-bakery test test-update-script
 
 rm -f bundles/slots-bundle/payloads/test-dir.tar
 tar -cvf bundles/slots-bundle/payloads/test-dir.tar -C bundles/slots-bundle/payloads test-file
 ./run-bakery bundler bundle bundles/slots-bundle build/slots-bundle.rugixb
+echo "TEST: test-update-custom-slots"
 ./run-bakery test test-update-custom-slots
